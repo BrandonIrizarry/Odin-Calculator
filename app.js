2f9099be9cc2c0c024affdf85ef61859a8f8fb50
Brandon C. Irizarry <brandon.irizarry@gmail.com>
2023-01-31 18:10:44 -0500

    Refactor decimal-point-active flag into factory-object
    The factory object 'calcState' now manages state for the calculator
    application. This is mainly to keep global-variable use under control.
    
    The DOM references have also all been moved to the top of the
    file. The main reason for this is to let them appear within the scope
    of 'initCalcState'. This way, the function can freely use whatever DOM
    references it should need going forward.

--- app.js
+++ app.js
@@ -1,35 +1,37 @@
 // @ts-check
 "use strict";
 
-// Ensure that a DOM element is non-null.
-// If all is well, return the same element.
-function assertElement (element = new Element()) {
-    if (element == null) {
-	throw new Error("display-element is null");
-    }
-
-    return element;
-}
-
-function assertElementCollection (collection = new NodeList()) {
-    if (collection.length === 0) {
-	throw new Error("empty element collection");
-    }
-
-    return collection;
-}
-
-// Ensure that a given numerical value isn't NaN.
-// If all is well, return the same number.
-function assertNotNaN (number = 0) {
-    if (isNaN(number)) {
-	throw new Error("parameter is NaN");
-    }
-
-    return number;
-}
-
 const display = assertElement(document.querySelector(".entry"));
+const decimalPointButton = assertElement(document.querySelector(".area-numberpad > .decimal-point"));
+const numberButtons = assertElementCollection(document.querySelectorAll(".area-numberpad > .js-number"));
+const backspaceButton = assertElement(document.querySelector("#js-backspace"));
+const arithmeticButtons = assertElementCollection(document.querySelectorAll(".area-arithmetic > *"));
+
+function initCalcState () {
+    let decimalPointActive = false;
+
+    function activateDecimalPoint () {
+	decimalPointActive = true;
+	decimalPointButton.classList.add("js-active");
+    }
+
+    function deactivateDecimalPoint () {
+	decimalPointActive = false;
+	decimalPointButton.classList.remove("js-active");
+    }
+
+    function decimalPointIsActive () {
+	return decimalPointActive;
+    }
+
+    return {
+	activateDecimalPoint,
+	deactivateDecimalPoint,
+	decimalPointIsActive,
+    };
+}
+
+const calcState = initCalcState();
 
 // Read the string occupying the display, and convert it to
 // fixed-point format.
@@ -78,45 +80,32 @@ function writeValue (fixedPointNumber = {}) {
     display.textContent = resultChars.join("");
 }
 
-// NUMBER BUTTONS ACTION
+// EVENT LISTENERS
 
-const numberButtons = assertElementCollection(document.querySelectorAll(".area-numberpad > .js-number"));
-
-let doScaleIncrease = false;
-
+// Number buttons
 numberButtons.forEach(numberButton => numberButton.addEventListener("click", () => {
     const digit = assertNotNaN(parseInt(numberButton.textContent));
     let { result, scale } = readValue();
 
     result = result * 10 + digit;
 
-    if (doScaleIncrease) {
+    if (calcState.decimalPointIsActive()) {
 	scale++;
     }
 
     writeValue({ result, scale });
 }));
 
-// DECIMAL-POINT BUTTON ACTION
+// Decimal point button
+decimalPointButton.addEventListener("click", calcState.activateDecimalPoint);
 
-const decimalPointButton = assertElement(document.querySelector(".area-numberpad > .decimal-point"));
-
-decimalPointButton.addEventListener("click", () => {
-    doScaleIncrease = true;
-    decimalPointButton.classList.add("js-active");
-});
-
-// BACKSPACE BUTTON ACTION
-
-const backspaceButton = assertElement(document.querySelector("#js-backspace"));
-
+// Backspace button
 backspaceButton.addEventListener("click", () => {
     let { result, scale } = readValue();
 
     // Deleting past the decimal point should deactivate it
     if (scale === 0) {
-	doScaleIncrease = false;
-	decimalPointButton.classList.remove("js-active");
+	calcState.deactivateDecimalPoint();
     }
 
     // Use integer division to eliminate the current ones digit
@@ -128,6 +117,32 @@ backspaceButton.addEventListener("click", () => {
     writeValue({ result, scale });
 });
 
-// OPERATOR BUTTONS ACTION
+// ASSERT-GUARD DEFINITIONS
 
-const arithmeticButtons = assertElementCollection(document.querySelectorAll(".area-arithmetic > *"));
+// Ensure that a DOM element is non-null.
+// If all is well, return the same element.
+function assertElement (element = new Element()) {
+    if (element == null) {
+	throw new Error("display-element is null");
+    }
+
+    return element;
+}
+
+function assertElementCollection (collection = new NodeList()) {
+    if (collection.length === 0) {
+	throw new Error("empty element collection");
+    }
+
+    return collection;
+}
+
+// Ensure that a given numerical value isn't NaN.
+// If all is well, return the same number.
+function assertNotNaN (number = 0) {
+    if (isNaN(number)) {
+	throw new Error("parameter is NaN");
+    }
+
+    return number;
+}
